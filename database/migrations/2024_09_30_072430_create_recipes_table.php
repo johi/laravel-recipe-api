b845ffbae44d1a032382b6227308227fd4b7808f
Johan Lerche Schulz <johan.schulz@gmail.com>
2024-09-30 13:43:48 +0200

    initial codebase, only added some extra migrations

---
+++ database/migrations/2024_09_30_072430_create_recipes_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
+
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('title');
+            $table->string('description');
+            $table->string('preparation_time');
+            $table->string('servings');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipe_id')->constrained('recipes');
+            $table->string('title');
+            $table->float('quantity');
+            $table->string('unit');
+            $table->timestamps();
+        });
+
+        Schema::create('instructions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipe_id')->constrained('recipes');
+            $table->string('description');
+            $table->unsignedInteger('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instructions');
+        Schema::dropIfExists('ingredients');
+        Schema::dropIfExists('recipes');
+        Schema::dropIfExists('categories');
+    }
+};
