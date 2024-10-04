5709c46657ba678acd99fd696e23992bb1fd8d5d
Johan Lerche Schulz <johan.schulz@gmail.com>
2024-10-04 10:47:18 +0200

    made authentication work, deleting token and setting timeout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ApiLoginRequest;
+use App\Http\Requests\Api\LoginUserRequest;
 use App\Models\User;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
@@ -12,16 +12,26 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login(ApiLoginRequest $request) {
-        return $this->ok($request->get('email'), $request->get('password'));
-//        $request->validated($request->all());
-//
-//        if (!Auth::attempt($request->only('email', 'password'))) {
-//            return $this->error('Invalid credentials', 401);
-//        }
-//
-//        $user = User::firstWhere('email', $request->email);
-//
+    public function login(LoginUserRequest $request) {
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalid credentials', 401);
+        }
+
+        $user = User::firstWhere('email', $request->email);
+
+        return $this->ok('Authenticated',
+            [
+                'token' => $user
+                    ->createToken(
+                        'API Token for ' . $user->email,
+                        ['*'],
+                        now()->addMonth()
+                    )
+                    ->plainTextToken,
+            ]
+        );
 //        return $this->ok(
 //            'Authenticated',
 //            [
@@ -35,5 +45,10 @@ class AuthController extends Controller
 
     public function register() {
         return $this->ok('Hello Register');
+    }
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('');
     }
 }
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\RecipeController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::apiResource('recipes', RecipeController::class);
+Route::middleware('auth:sanctum')->apiResource('recipes', RecipeController::class);
 
 Route::get('/categories', [CategoryController::class, 'index']);
