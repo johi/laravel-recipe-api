24b154dcae4845154edfbe4231c4d020f5219aba
Johan Lerche Schulz <johan.schulz@gmail.com>
2024-10-12 18:05:45 +0200

    refactored a bit on AuthorsRecipesController

--- app/Http/Controllers/Api/V1/AuthorsRecipesController.php
+++ app/Http/Controllers/Api/V1/AuthorsRecipesController.php
@@ -30,59 +30,43 @@ class AuthorsRecipesController extends ApiController
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRecipeRequest $request, int $author_id)
+    public function store(StoreRecipeRequest $request)
     {
-        try {
-            $category = Category::findOrFail($request->input('data.relationships.category.data.id'));
-            if ($this->isAble('store', Recipe::class)) {
-                return new RecipeResource(Recipe::create($request->mappedAttributes([
-                    'author' => 'user_id'
-                ])));
-            }
-            return $this->error('You are not authorized to create that resource', 401);
-        } catch (ModelNotFoundException $exception) {
-            return $this->error(sprintf('%s cannot be found.', class_basename($exception->getModel())), 400);
+        $category = Category::findOrFail($request->input('data.relationships.category.data.id'));
+        if ($this->isAble('store', Recipe::class)) {
+            return new RecipeResource(Recipe::create($request->mappedAttributes([
+                'author' => 'user_id'
+            ])));
         }
+        return $this->error('You are not authorized to create that resource', 401);
     }
 
     public function update(UpdateRecipeRequest $request, int $author_id, int $recipe_id)
     {
-        try {
-            $recipe = Recipe::where('id', $recipe_id)
-                ->where('user_id', $author_id)
-                ->firstOrFail();
-            if ($this->isAble('update', $recipe)) {
-                $mappedAttributes = $request->mappedAttributes();
-                if (isset($mappedAttributes['category_id'])) {
-                    $category = Category::findOrFail($mappedAttributes['category_id']);
-                }
-                $recipe->update($mappedAttributes);
-                return new RecipeResource($recipe);
+        $recipe = Recipe::where('id', $recipe_id)
+            ->where('user_id', $author_id)
+            ->firstOrFail();
+        if ($this->isAble('update', $recipe)) {
+            $mappedAttributes = $request->mappedAttributes();
+            if (isset($mappedAttributes['category_id'])) {
+                $category = Category::findOrFail($mappedAttributes['category_id']);
             }
-            return $this->error('You are not authorized to update that resource', 401);
-        } catch (ModelNotFoundException $exception) {
-            $className = class_basename($exception->getModel());
-            return $this->error(
-                sprintf('%s cannot be found.', $className),
-                ($className === 'Recipe') ? 404 : 400
-            );
+            $recipe->update($mappedAttributes);
+            return new RecipeResource($recipe);
         }
+        return $this->error('You are not authorized to update that resource', 401);
     }
 
     public function replace(ReplaceRecipeRequest $request, int $author_id, int $recipe_id)
     {
-        try {
-            $recipe = Recipe::where('id', $recipe_id)
-                ->where('user_id', $author_id)
-                ->firstOrFail();
-            if ($this->isAble('replace', $recipe)) {
-                $recipe->update($request->mappedAttributes());
-                return new RecipeResource($recipe);
-            }
-            return $this->error('You are not authorized to update that resource', 401);
-        } catch (ModelNotFoundException $exception) {
-            return $this->error('Recipe cannot be found.', 404);
+        $recipe = Recipe::where('id', $recipe_id)
+            ->where('user_id', $author_id)
+            ->firstOrFail();
+        if ($this->isAble('replace', $recipe)) {
+            $recipe->update($request->mappedAttributes());
+            return new RecipeResource($recipe);
         }
+        return $this->error('You are not authorized to update that resource', 401);
     }
 
     /**
@@ -90,17 +74,13 @@ class AuthorsRecipesController extends ApiController
      */
     public function destroy(int $author_id, int $recipe_id)
     {
-        try {
-            $recipe = Recipe::where('id', $recipe_id)
-                ->where('user_id', $author_id)
-                ->firstOrFail();
-            if ($this->isAble('delete', $recipe)) {
-                $recipe->delete();
-                return $this->ok('Recipe successfully deleted');
-            }
-            return $this->error('You are not authorized to delete that resource', 401);
-        } catch (ModelNotFoundException $exception) {
-            return $this->error('Recipe cannot be found.', 404);
+        $recipe = Recipe::where('id', $recipe_id)
+            ->where('user_id', $author_id)
+            ->firstOrFail();
+        if ($this->isAble('delete', $recipe)) {
+            $recipe->delete();
+            return $this->ok('Recipe successfully deleted');
         }
+        return $this->error('You are not authorized to delete that resource', 401);
     }
 }
