ea8b685342285883e2f25d5caa0022971d787d29
Johan Lerche Schulz <johan.schulz@gmail.com>
2024-10-23 11:29:59 +0200

    added missing AuthorsControllerTest tests

--- tests/Feature/V1/AuthorsControllerTest.php
+++ tests/Feature/V1/AuthorsControllerTest.php
@@ -25,19 +25,7 @@ class AuthorsControllerTest extends TestCase
         $response->assertStatus(200)
             ->assertJsonStructure([
                 'data' => [
-                    0 => [
-                        'type',
-                        'id',
-                        'attributes' => [
-                            'name',
-                            'email',
-                            'isAdmin',
-                            'emailVerifiedAt',
-                            'createdAt',
-                            'updatedAt',
-                            'included'
-                        ]
-                    ]
+                    0 => $this->getAuthorStructure()
                 ],
                 'links',
                 'meta',
@@ -51,6 +39,37 @@ class AuthorsControllerTest extends TestCase
         $response = $this->get(self::ENDPOINT_PREFIX . '/authors?include=recipes');
         $response->assertStatus(200)
             ->assertJsonCount(AuthorsControllerSeeder::RECIPES_TO_CREATE, 'data.0.attributes.included.recipes');
+    }
+
+    public function test_as_anonymous_i_get_a_single_author(): void
+    {
+        $response = $this->get(self::ENDPOINT_PREFIX . '/authors/1');
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data' => $this->getAuthorStructure()]);
+    }
+
+    public function test_i_can_include_recipes_for_single_author(): void
+    {
+        $response = $this->get(self::ENDPOINT_PREFIX . '/authors/1?include=recipes');
+        $response->assertStatus(200)
+            ->assertJsonCount(AuthorsControllerSeeder::RECIPES_TO_CREATE, 'data.attributes.included.recipes');
+    }
+
+    private function getAuthorStructure(): array
+    {
+        return [
+            'type',
+            'id',
+            'attributes' => [
+                'name',
+                'email',
+                'isAdmin',
+                'emailVerifiedAt',
+                'createdAt',
+                'updatedAt',
+                'included'
+            ]
+        ];
     }
 
 }
