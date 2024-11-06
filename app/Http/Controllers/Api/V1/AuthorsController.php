a81efbec3e7701c8b2c3f9c9bcf3499748bf838d
Johan Lerche Schulz <johan.schulz@gmail.com>
2024-11-06 08:51:29 +0100

    documentation for AuthorsController

--- app/Http/Controllers/Api/V1/AuthorsController.php
+++ app/Http/Controllers/Api/V1/AuthorsController.php
@@ -14,7 +14,22 @@ class AuthorsController extends ApiController
     /**
      * Get all authors
      *
+     * This retrieves users having published recipes only. Please refer to laravel documentation on how
+     * to use pagination: https://laravel.com/docs/11.x/pagination
+     *
      * @group Authors
+     * @queryParam filter[createdAt] Filter by created date (iso: YYYY-MM-DD)  Example: exact date
+     * filter[createdAt]=2024-10-13 or between dates filter[createdAt]=2024-10-13,2024-11-13
+     * @queryParam filter[updatedAt] Filter by updated date (iso: YYYY-MM-DD) Example: exact date
+     * filter[updatedAt]=2024-10-13 or between dates filter[updatedAt]=2024-10-13,2024-11-13
+     * @queryParam filter[id] Filter by comma separated list of id's Example: filter[id]=1,2,3
+     * @queryParam filter[email] Filter by email, with or without using wildcard `*` Example:
+     * filter[email]=`*`@example.com
+     * @queryParam filter[name] Filter by name, works with or without use of wildcard Example:
+     * filter[name]=`*`Miller
+     * @queryParam include Include related resources, possible values: recipes Example: include=recipes
+     * @queryParam sort Data field(s) to sort by: name, email, createdAt, updatedAt. Separate multiple
+     * with commas. Denote descending sort with a minus sign. Example: sort: name,-createdAt
      */
     public function index(AuthorFilter $filters)
     {
@@ -31,6 +46,7 @@ class AuthorsController extends ApiController
      * Get single author
      *
      * @group Authors
+     * @queryParam include Include related resources, possible values: recipes Example: include=recipes
      */
     public function show(int $user_id)
     {
--- app/Http/Controllers/Api/V1/AuthorsRecipesController.php
+++ app/Http/Controllers/Api/V1/AuthorsRecipesController.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Filters\V1\RecipeFilter;
-use App\Http\Requests\Api\V1\ReplaceRecipeRequest;
-use App\Http\Requests\Api\V1\StoreRecipeRequest;
-use App\Http\Requests\Api\V1\UpdateRecipeRequest;
 use App\Http\Resources\V1\RecipeResource;
-use App\Models\Category;
 use App\Models\Recipe;
 use App\Policies\V1\RecipePolicy;
-use Illuminate\Support\Facades\Gate;
 
 class AuthorsRecipesController extends ApiController
 {
@@ -21,11 +16,29 @@ class AuthorsRecipesController extends ApiController
     /**
      * Get authors recipes
      *
+     * Retrieve all recipes for an author. Please refer to laravel documentation on how
+     *  to use pagination: https://laravel.com/docs/11.x/pagination
+     * 
      * @group Authors
+     * @queryParam filter[createdAt] Filter by created date (iso: YYYY-MM-DD)  Example: exact date
+     *  filter[createdAt]=2024-10-13 or between dates filter[createdAt]=2024-10-13,2024-11-13
+     * @queryParam filter[updatedAt] Filter by updated date (iso: YYYY-MM-DD) Example: exact date
+     *  filter[updatedAt]=2024-10-13 or between dates filter[updatedAt]=2024-10-13,2024-11-13
+     * @queryParam filter[preparationTimeMinutes] Filter by preparationTimeMinutes Example: less
+     * than or equal to filter[preparationTimeMinutes]=30 or between filter[preparationTimeMinutes]=15,45
+     * @queryParam filter[title] Filter by title, works with or without use of wildcard Example:
+     *  filter[title]=`*`Muffins
+     * @queryParam include Include related possible values: category, ingredients, instructions
+     * Example: include=instructions,recipes
+     * @queryParam sort Data field(s) to sort by: title, preparationTimeMinutes, createdAt, updatedAt.
+     * Separate multiple with commas. Denote descending sort with a minus sign. Example: sort: name,-createdAt
      */
     public function index(RecipeFilter $filters, int $authorId) {
         return RecipeResource::collection(
-            Recipe::where('user_id', $authorId)->with($this->includes($this->possibleIncludes))->filter($filters)->paginate()
+            Recipe::where('user_id', $authorId)
+                ->with($this->includes($this->possibleIncludes))
+                ->filter($filters)
+                ->paginate()
         );
     }
 }
