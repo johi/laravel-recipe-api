51b5955145da115c5ab249ff46b2127e77c96a6b
Johan Lerche Schulz <johan.schulz@gmail.com>
2024-12-04 13:17:26 +0100

    refactored AuthorsControllerTest

--- tests/Feature/V1/AuthorsControllerTest.php
+++ tests/Feature/V1/AuthorsControllerTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Feature\V1;
 
-use Database\Seeders\Tests\V1\AuthorsControllerSeeder;
+use App\Models\Recipe;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -10,16 +11,14 @@ class AuthorsControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    const ENDPOINT_PREFIX = 'api/v1';
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->seed(AuthorsControllerSeeder::class);
-    }
-
     public function test_as_anonymous_i_get_a_list_of_all_authors(): void
     {
-        $response = $this->get(self::ENDPOINT_PREFIX . '/authors');
+        $user = User::factory()->create();
+        Recipe::factory()->create(['user_id' => $user->id]);
+        $anotherUser = User::factory()->create();
+        Recipe::factory()->create(['user_id' => $anotherUser->id]);
+        $thirdUser = User::factory()->create(); // no associated recipes
+        $response = $this->getJson(route('authors.index'));
         $response->assertStatus(200)
             ->assertJsonStructure([
                 'data' => [
@@ -28,35 +27,40 @@ class AuthorsControllerTest extends TestCase
                 'links',
                 'meta',
             ])
-            ->assertJsonPath('meta.total', AuthorsControllerSeeder::USERS_TO_CREATE)
-            ->assertJsonCount(0, 'data.0.attributes.included');
+            ->assertJsonPath('meta.total', 2);
     }
 
     public function test_i_can_include_recipes_for_list_of_all_authors(): void
     {
-        $response = $this->get(self::ENDPOINT_PREFIX . '/authors?include=recipes');
+        $user = User::factory()->create();
+        Recipe::factory(3)->create(['user_id' => $user->id]);
+        $response = $this->getJson(route('authors.index',['include' => 'recipes',]));
         $response->assertStatus(200)
-            ->assertJsonCount(AuthorsControllerSeeder::RECIPES_TO_CREATE, 'data.0.attributes.included.recipes');
+            ->assertJsonCount(3, 'data.0.attributes.included.recipes');
     }
 
     public function test_as_anonymous_i_get_a_single_author(): void
     {
-        $response = $this->get(self::ENDPOINT_PREFIX . '/authors/1');
+        // @todo make sure an author has at least one recipe
+        $user = User::factory()->create();
+        $response = $this->getJson(route('authors.show',$user));
         $response->assertStatus(200)
             ->assertJsonStructure(['data' => $this->getAuthorStructure()]);
     }
 
     public function test_trying_to_show_non_existent_author_gives_404(): void
     {
-        $response = $this->get(self::ENDPOINT_PREFIX . '/authors/1000');
+        $response = $this->getJson(route('authors.show','1000'));
         $response->assertStatus(404);
     }
 
     public function test_i_can_include_recipes_for_single_author(): void
     {
-        $response = $this->get(self::ENDPOINT_PREFIX . '/authors/1?include=recipes');
+        $user = User::factory()->create();
+        Recipe::factory(3)->create(['user_id' => $user->id]);
+        $response = $this->getJson(route('authors.show',[$user, 'include' => 'recipes',]));
         $response->assertStatus(200)
-            ->assertJsonCount(AuthorsControllerSeeder::RECIPES_TO_CREATE, 'data.attributes.included.recipes');
+            ->assertJsonCount(3, 'data.attributes.included.recipes');
     }
 
     private function getAuthorStructure(): array
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,49 @@
 
 namespace Tests;
 
+use App\Http\Controllers\Api\AuthController;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function makeAuthenticatedRequestWithToken(User $user, string $method, string $route, array $data = []): \Illuminate\Testing\TestResponse
+    {
+        // Generate a token for the user
+        $token = AuthController::createToken($user);
+
+        if ($method === 'DELETE') {
+            return $this->deleteJson($route, $data, [
+                'Authorization' => "Bearer $token"
+            ]);
+        }
+        // Make the authenticated request
+        return $this->withHeaders([
+            'Authorization' => "Bearer $token",
+        ])->json($method, $route, $data);
+    }
+
+    protected function getAuthenticatedUserJsonGet(User $user, string $route, array $data = []): \Illuminate\Testing\TestResponse
+    {
+        return $this->makeAuthenticatedRequestWithToken($user, 'GET', $route, $data);
+    }
+    protected function getAuthenticatedJsonPost(User $user, string $route, array $data = []): \Illuminate\Testing\TestResponse
+    {
+        return $this->makeAuthenticatedRequestWithToken($user, 'POST', $route, $data);
+    }
+
+    protected function getAuthenticatedJsonPut(User $user, string $route, array $data = []): \Illuminate\Testing\TestResponse
+    {
+        return $this->makeAuthenticatedRequestWithToken($user, 'PUT', $route, $data);
+    }
+
+    protected function getAuthenticatedJsonPatch(User $user, string $route, array $data = []): \Illuminate\Testing\TestResponse
+    {
+        return $this->makeAuthenticatedRequestWithToken($user, 'PATCH', $route, $data);
+    }
+
+    protected function getAuthenticatedJsonDelete(User $user, string $route, array $data = []): \Illuminate\Testing\TestResponse
+    {
+        return $this->makeAuthenticatedRequestWithToken($user, 'DELETE', $route, $data);
+    }
 }
